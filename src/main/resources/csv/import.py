28d04e1b32e7ecc4568cd3897a64dea1a8ae7cbf
Vincent Lussenburg <vlussenburg@xebia.com>
2018-01-29 15:32:31 +0100

    refactor

--- src/main/resources/csv/import.py
+++ src/main/resources/csv/import.py
@@ -16,21 +16,65 @@ from com.xebialabs.xlrelease.domain import Release
 from com.xebialabs.xlrelease.domain.status import ReleaseStatus
 from com.xebialabs.xlrelease.api.v1.views import TeamView
 
+phase_name_id_map = {}
+
 def create_team(teamName, id=None):
     teamView = TeamView()
     teamView.id = id
     teamView.teamName = teamName
     return teamView
 
-def add_teams_to_template(template, teamNames):
-    teams = []
-    teams.append(create_team(template['teams'][0]['teamName'], template['teams'][0].id))
-    teams.append(create_team(template['teams'][1]['teamName'], template['teams'][1].id))
+def add_teams_to_template(template, tasks):
+    unique_teams = Set([task['team'] for task in tasks])
+    unique_teams.remove('')
 
-    for teamName in teamNames:
-        teams.append(create_team(teamName))
+    teams = []
+    for team in template['teams']:
+        teams.append(create_team(team['teamName'], team.id))
+
+    for team_name in unique_teams:
+        teams.append(create_team(team_name))
 
     templateApi.setTeams(template.id, teams)
+
+def add_phase_if_not_exists_and_return_id(template, phase_name):
+    if phase_name not in phase_name_id_map.keys():
+        phase = phaseApi.addPhase(template.id, phaseApi.newPhase(phase_name))
+        phase_name_id_map[phase_name] = phase.id
+
+    return phase_name_id_map[phase_name]
+
+def parse_csv(csv_str):
+    tasks = []
+    template_reader = csv.reader(csv_str.split('\n'), delimiter='\t', dialect='excel', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+    for row in template_reader:
+        task = {}
+        task['phase_name'] = row[0]
+        task['title'] = row[1]
+        task['description'] = row[5]
+        task['team'] = row[6]
+        
+        tasks.append(task)
+
+    return tasks
+
+def create_tasks(template, tasks):    
+    for task in tasks:
+        phase_name = task['phase_name']
+        title = task['title']
+        description = task['description']
+        team = task['team']
+        
+        phase_id = add_phase_if_not_exists_and_return_id(template, phase_name)
+
+        task = taskApi.newTask()
+        task.description = description
+        task.title = title
+
+        if team:
+            task.team = team
+
+        phaseApi.addTask(phase_id, task, None)
 
 for item in request.entity:
     if item['name'] == 'csv':
@@ -49,36 +93,10 @@ try:
     # Delete unnecessary "New Phase"
     phaseApi.deletePhase(template['phases'][0].id)
 
-    phases = []
-    phase_name_id_map = {}
-    template_reader = csv.reader(csv_str.split('\n'), delimiter='\t', dialect='excel', quotechar='"', quoting=csv.QUOTE_MINIMAL)
-    for row in template_reader:
-        if not row[0] in phases:
-            phase_name = row[0]
-            phases.append(phase_name)
-            phase = phaseApi.addPhase(template.id, phaseApi.newPhase(phase_name))
-            phase_name_id_map[phase_name] = phase.id
-
     # Create Tasks
-    teamNames = Set([])
-    template_reader = csv.reader(csv_str.split('\n'), delimiter='\t', dialect='excel', quotechar='"', quoting=csv.QUOTE_MINIMAL)
-    for row in template_reader:
-        phase_id = phase_name_id_map[row[0]]
-        title = row[1]
-        description = row[5]
-        team = row[6]
-
-        task = taskApi.newTask()
-        task.description = description
-        task.title = title
-
-        if team:
-            task.team = team
-            teamNames.add(team)
-
-        phaseApi.addTask(phase_id, task, None)
-
-    add_teams_to_template(template, teamNames)
+    tasks = parse_csv(csv_str)
+    create_tasks(template, tasks)
+    add_teams_to_template(template, tasks)
 
     response.statusCode = 200
     response.entity = json.dumps({"result": "Successfully imported template [%s]" % template_name})
